5cd815a844ff6191902845ae1b28648d19829cb3
FMiravete <flavio.miravete@gmail.com>
2024-03-17 00:19:24 -0300

    borrados:        build/test/cache/defines_dependency.yml 	borrados:        build/test/cache/input.yml 	borrados:        build/test/cache/test_leds.c 	borrados:        build/test/dependencies/cmock.d 	borrados:        build/test/dependencies/leds.d 	borrados:        build/test/dependencies/test_leds.d 	borrados:        build/test/dependencies/test_leds_runner.d 	borrados:        build/test/dependencies/unity.d 	borrados:        build/test/out/c/cmock.o 	borrados:        build/test/out/c/leds.o 	borrados:        build/test/out/c/test_leds.o 	borrados:        build/test/out/c/test_leds_runner.o 	borrados:        build/test/out/c/unity.o 	borrados:        build/test/out/test_leds.out 	borrados:        build/test/preprocess/files/test_leds.c 	borrados:        build/test/preprocess/includes/test_leds.c 	borrados:        build/test/results/test_leds.pass 	borrados:        build/test/runners/test_leds_runner.c 	modificados:     src/leds.c

--- src/leds.c
+++ src/leds.c
@@ -1,20 +1,29 @@
 #include "leds.h"
 
-static uint16_t * puntero;
+#define ALL_LEDS_OFF 0x00
+#define BIT_HIGH 1
+#define LED_OFFSET 1
+
+static uint16_t * puntero; 
+
+static uint16_t led_to_mask(int led)
+{
+    return (BIT_HIGH << (led - LED_OFFSET));
+}
 
 void leds_init(uint16_t * port)
 {
     puntero = port;
-    *puntero = 0x00;
+    *puntero = ALL_LEDS_OFF;
 }
 
 
 void leds_turn_on(int led)
 {
-    *puntero |= (1 << (led - 1));
+    *puntero |= led_to_mask(led);
 }
 
 void leds_turn_off(int led)
 {
-    *puntero &= ~(1 << (led - 1));
+    *puntero &= ~led_to_mask(led);
 }
